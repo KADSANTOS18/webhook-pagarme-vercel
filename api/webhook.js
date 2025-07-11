aae1df42742a1698e54a314ed8d61e1df33a0ae0
SLOWBOX <KADSANTOS.KS@GMAIL.COM>
2025-07-11 15:49:32 -0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -26,44 +26,4 @@ export default async function handler(req, res) {
           codigo: "PROD001",
           descricao: "Produto Teste",
           quantidade: 1,
-          valor: body.amount / 100
-        }
-      ],
-      formaPagamento: {
-        id: 1
-      },
-      tipoIntegracao: "API"
-    };
 
-    try {
-      const response = await fetch('https://www.bling.com.br/Api/v3/pedidos', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic SEU_TOKEN_ENCODED'
-        },
-        body: JSON.stringify(pedido)
-      });
-
-      const data = await response.json();
-
-      return res.status(200).json({
-        success: true,
-        message: 'Pedido enviado ao Bling com sucesso.',
-        blingResponse: data
-      });
-
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: 'Erro ao enviar para o Bling.',
-        error: error.message
-      });
-    }
-  } else {
-    return res.status(200).json({
-      success: true,
-      message: 'Evento ignorado: ' + body.event
-    });
-  }
-}
